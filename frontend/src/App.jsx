887afc6f3f22b06bfd455818a15212f6eec00f90
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2025-11-26 16:17:19 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,16 @@
-import React, { useContext, Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Home } from "lucide-react";
-import Login from "./Pages/Login";
-import AppContext from "./Context/UseContext";
-import InstitutionDashboard from "./Pages/Institution/InstitutionDashboard";
-import InstitutionProfile from "./Pages/Institution/InstitutionProfile";
-import ParametersEntry from "./Pages/Institution/ParametersEntry";
-import DocumentUpload from "./Pages/Institution/DocumentUpload";
-import ApplicationSubmission from "./Pages/Institution/ApplicationSubmission";
-import Queries from "./Pages/Institution/Queries";
-import Reviews from "./Pages/Institution/Reviews";
-import AIAnalytics from "./Pages/Institution/AiAnalytics";
-import AIReports from "./Pages/Institution/AIReports";
-
-
+import React, { useContext , Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from 'lucide-react';
+import Login from './Pages/Login';
+import AppContext from './Context/UseContext';
+import Dashboard from './Pages/Dashboard';
 
 const App = () => {
   const { auth } = useContext(AppContext);
-  console.log(auth);
+  console.log(auth)
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<InstitutionDashboard/>} />
@@ -32,9 +22,6 @@ const App = () => {
           <Route path="/reviews" element={<Reviews/>} />
           <Route path="/ai-analytics" element={<AIAnalytics />} />
           <Route path="/ai-reports" element={<AIReports />} />
-
-
-         
         </Routes>
       </Suspense>
     </div>
